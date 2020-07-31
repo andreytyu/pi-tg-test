4e0c1aba2985c210292714fe3d07895a28f06a1d
Andrey Tyukavin <geotyukavin@gmail.com>
2020-07-31 19:06:29 +0300

    utf8 fix

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ def cpu_temp_check(update, context):
     bash_command = 'vcgencmd measure_temp'
     process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
-    update.message.reply_text(str(output))
+    update.message.reply_text(output.decode("utf-8"))
 
 
 def main():
